152b2cfe5f47184c1377797fbd58c3ac3615fa5a
Adam Jacques <adam@technowizardry.net>
2018-10-18 08:57:17 -0700

    Add project files.

---
+++ KubeUpdateCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using k8s;
+
+namespace KubeUpdateCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("./kubeconfig.yaml");
+            IKubernetes kubernetes = new Kubernetes(config);
+
+            var result = kubernetes.ListNamespace();
+            result.ToString();
+        }
+    }
+}
